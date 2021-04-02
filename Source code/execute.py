f6f7eeb4deb6e2dbfc8e32bc94d787fa652c6696
Nachiketh B M <70973171+Nachiketh2001@users.noreply.github.com>
2021-04-03 00:44:28 +0530

    Update execute.py

--- Source code/execute.py
+++ Source code/execute.py
@@ -1,24 +1,31 @@
+import numpy as np
+import Route_Finding_Algorithm
+
 # Getting inputs
 
-n = int(input("Enter number of cities(a positive integer): "))
-e = input("Enter distance between cities(a 2D list with single space as a separator): ")
+n = int(input("Enter number of cities: "))
+e = np.array(input("Enter distance between cities: ").split(), float)
+e = e.reshape(n, n)
 
 k = int(input("Enter number of electric vehicles: "))
 
 # s_node-source node   d_node-destination node   b-battery charge status initially
 # c-charging rate   d-discharging rate   m-maximum battery capacity   s-average travelling speed
 
-s_node = input("Enter source node: ")
-d_node = input("Enter destination node: ")
-b = input("Enter initial battery charge status: ")
-c = input("Enter charging rate: ")
-d = input("Enter discharging rate: ")
-m = input("Enter maximum battery capacity: ")
-s = input("Enter average travelling speed: ")
+s_node = np.array(input("Enter source node: ").split(), int)
+d_node = np.array(input("Enter destination node: ").split(), int)
 
-# Applying optimal algorithm
+initial_battery = np.array(input("Enter initial battery charge status: ").split(), float)
+# unit: kW hr
+charging = np.array(input("Enter charging rate: ").split(), float)
+# unit: kW
+discharging = np.array(input("Enter discharging rate: ").split(), float)
+# unit: kW
+maximum_battery = np.array(input("Enter maximum battery capacity: ").split(), float)
+# unit: kW hr
+speed = np.array(input("Enter average travelling speed: ").split(), float)
+# unit: km/hr
 
-optimal(n, e, k, s_node, d_node, b, c, d, m, s)
+# Applying Route_Finding_Algorithm
 
-
-
+Route_Finding_Algorithm.find_route(n, e, k, s_node, d_node, initial_battery, charging, discharging, maximum_battery, speed)
