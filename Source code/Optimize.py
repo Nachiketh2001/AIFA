1e20d68931892ded7a232b06871e4c6cc82c7182
Nachiketh B M <70973171+Nachiketh2001@users.noreply.github.com>
2021-04-07 17:09:09 +0530

    Update Optimize.py

--- Source code/Optimize.py
+++ Source code/Optimize.py
@@ -1,6 +1,7 @@
 import numpy as np
 # This file minimises the total time taken by that vehicle which has the longest journey time
 
+
 def check_time(time_occupied, t_initial, t_final):
     station_free_at = t_initial
     t = t_initial
@@ -28,82 +29,86 @@ def optimize(n, time, required_time_start, required_time_middle, begin_time_midd
 
     i1 = 0
     while i1 < n:
-        if station_node[i1][0] is not None and len(station_node[i1][0]) > 1:
-            row = station_node[i1][0]
+        if station_node[i1][0] is not None:
+            if len(station_node[i1][0]) > 1:
+                row = station_node[i1][0]
 
-            priority_order = []
-            max_priority_vehicle = 0
-            vehicle_available = {}
-            for i2 in row:
-                vehicle_available[i2] = True
+                priority_order = []
+                max_priority_vehicle = 0
+                vehicle_available = {}
+                for i2 in row:
+                    vehicle_available[i2] = True
 
-            i2 = 0
-            while i2 < len(row):
-                i3 = 0
-                while i3 < len(row):
-                    if vehicle_available.get(i3):
-                        max_priority_vehicle = row[i3]
-                        break
-                    i3 = i3 + 1
+                i2 = 0
+                while i2 < len(row):
+                    i3 = 0
+                    while i3 < len(row):
+                        if vehicle_available.get(row[i3]):
+                            max_priority_vehicle = row[i3]
+                            break
+                        i3 = i3 + 1
 
-                i3 = 0
-                while i3 < len(row):
-                    if vehicle_available.get(i3) and time[row[i3]] > time[max_priority_vehicle]:
-                        max_priority_vehicle = row[i3]
-                    i3 = i3 + 1
+                    i3 = 0
+                    while i3 < len(row):
+                        if vehicle_available.get(row[i3]) and time[row[i3]] > time[max_priority_vehicle]:
+                            max_priority_vehicle = row[i3]
+                        i3 = i3 + 1
 
-                vehicle_available[max_priority_vehicle] = False
-                priority_order = priority_order + [max_priority_vehicle]
-                i2 = i2 + 1
+                    vehicle_available[max_priority_vehicle] = False
+                    priority_order = priority_order + [max_priority_vehicle]
+                    i2 = i2 + 1
 
-            add_time = 0
-            for i2 in priority_order:
-                time[i2] = time[i2] + add_time
-                add_time = add_time + required_time_start[i2]
+                add_time = 0
+                for i2 in priority_order:
+                    time[i2] = time[i2] + add_time
+                    add_time = add_time + required_time_start[i2]
 
-        if station_node[i1][1] is not None and len(station_node[i1][0]) > 1:
-            row = station_node[i1][1]
+        if station_node[i1][1] is not None:
+            if len(station_node[i1][1]) > 1:
+                row = station_node[i1][1]
 
-            priority_order = []
-            max_priority_vehicle = 0
-            vehicle_available = {}
-            for i2 in row:
-                vehicle_available[i2] = True
+                priority_order = []
+                max_priority_vehicle = 0
+                vehicle_available = {}
+                for i2 in row:
+                    vehicle_available[i2] = True
 
-            i2 = 0
-            while i2 < len(row):
-                i3 = 0
-                while i3 < len(row):
-                    if vehicle_available.get(row[i3]):
-                        max_priority_vehicle = row[i3]
-                        break
-                    i3 = i3 + 1
+                i2 = 0
+                while i2 < len(row):
+                    i3 = 0
+                    while i3 < len(row):
+                        if vehicle_available.get(row[i3]):
+                            max_priority_vehicle = row[i3]
+                            break
+                        i3 = i3 + 1
 
-                i3 = 0
-                while i3 < len(row):
-                    if vehicle_available.get(row[i3]) and time[row[i3]] > time[max_priority_vehicle]:
-                        max_priority_vehicle = row[i3]
-                    i3 = i3 + 1
+                    i3 = 0
+                    while i3 < len(row):
+                        if vehicle_available.get(row[i3]) and time[row[i3]] > time[max_priority_vehicle]:
+                            max_priority_vehicle = row[i3]
+                        i3 = i3 + 1
 
-                vehicle_available[max_priority_vehicle] = False
-                priority_order = priority_order + [max_priority_vehicle]
-                i2 = i2 + 1
+                    vehicle_available[max_priority_vehicle] = False
+                    priority_order = priority_order + [max_priority_vehicle]
+                    i2 = i2 + 1
 
-            # Assuming energy given per unit time at any station is not greater than 10 times
-            # of the energy lost per unit time during travelling(thinking practically)
-            t = 0
-            t_max = 10*3600*(time[priority_order[0]])
-            time_occupied = np.zeros(t_max, dtype=int)
-            while t < t_max:
-                time_occupied[t] = False
-                t = t + 1
+                # Assuming energy given per unit time at any station is not greater than 10 times
+                # of the energy lost per unit time during travelling(thinking practically)
+                t = 0
+                t_max = 10*3600*(time[priority_order[0]])
+                t_max = int(t_max - t_max % 1)
+                time_occupied = np.zeros(t_max)
+                while t < t_max:
+                    time_occupied[t] = False
+                    t = t + 1
 
-            i2 = 0
-            while i2 < len(row):
-                t_initial = begin_time_middle[row[i2]]
-                t_final = begin_time_middle[row[i2]] + required_time_middle[row[i2]]
-                time_occupied, t_initial_new = check_time(time_occupied, t_initial, t_final)
-                time[row[i2]] = time[row[i2]] + t_initial_new - t_initial
-                i2 = i2 + 1
+                i2 = 0
+                while i2 < len(row):
+                    t_initial = begin_time_middle[row[i2]]
+                    t_final = begin_time_middle[row[i2]] + required_time_middle[row[i2]]
+                    time_occupied, t_initial_new = check_time(time_occupied, t_initial, t_final)
+                    time[row[i2]] = time[row[i2]] + t_initial_new - t_initial
+                    i2 = i2 + 1
         i1 = i1 + 1
+
     return time
